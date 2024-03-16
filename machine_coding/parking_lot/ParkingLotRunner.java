bcadc9e5f8d025b05473ecdf92b4b6cfb7dace7f
bhavikdand <bhavik.dand_1@scaler.com>
2024-03-16 07:22:34 +0530

    Updated ParkingLotRunner to demo generateTicket

--- machine_coding/parking_lot/ParkingLotRunner.java
+++ machine_coding/parking_lot/ParkingLotRunner.java
@@ -1,9 +1,9 @@
 package machine_coding.parking_lot;
 
 import machine_coding.parking_lot.controllers.TicketController;
-import machine_coding.parking_lot.models.Gate;
-import machine_coding.parking_lot.models.GateType;
-import machine_coding.parking_lot.models.Operator;
+import machine_coding.parking_lot.dtos.GenerateTicketRequestDto;
+import machine_coding.parking_lot.dtos.GenerateTicketResponseDto;
+import machine_coding.parking_lot.models.*;
 import machine_coding.parking_lot.repositories.GateRepository;
 import machine_coding.parking_lot.repositories.ParkingLotRepository;
 import machine_coding.parking_lot.repositories.TicketRepository;
@@ -14,8 +14,7 @@ import machine_coding.parking_lot.services.TicketService;
 import machine_coding.parking_lot.strategies.spot_assignment.AssignSpotStrategy;
 import machine_coding.parking_lot.strategies.spot_assignment.NearestFirstSpotAssignmentStrategy;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ParkingLotRunner {
 
@@ -27,13 +26,49 @@ public class ParkingLotRunner {
         gate1.setOperator(new Operator());
         gate1.setId(1);
 
-        Map<Integer, Gate> gateMap = new HashMap<>(){{
+        Gate gate2 = new Gate();
+        gate2.setId(2);
+        gate2.setOperator(new Operator());
+        gate2.setName("4Z");
+        gate2.setGateType(GateType.EXIT);
+
+        Map<Integer, Gate> gateMap = new HashMap<Integer, Gate>(){{
             put(1, gate1);
+            put(2, gate2);
+        }};
+
+        List<Spot> spots = Arrays.asList(new Spot("1A", SpotStatus.UNOCCUPIED, VehicleType.CAR)
+                //, new Spot("1B", SpotStatus.UNOCCUPIED, VehicleType.CAR)
+                );
+        List<Section> sections = new ArrayList<>();
+        Section section = new Section();
+        section.setName("AA");
+        section.setId(1);
+        section.setSpots(spots);
+        sections.add(section);
+
+        List<Floor> floors = new ArrayList<>();
+        Floor floor = new Floor();
+        floor.setFloorNum(1);
+        floor.setFloorStatus(FloorStatus.OPERATIONAL);
+        floor.setSections(sections);
+        floor.setId(1);
+        floors.add(floor);
+
+        List<Gate> gates = Arrays.asList(gate1, gate2);
+
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setFloors(floors);
+        parkingLot.setGates(gates);
+        parkingLot.setId(1);
+
+        Map<Integer, ParkingLot> parkingLotMap = new HashMap<Integer, ParkingLot>(){{
+            put(1, parkingLot);
         }};
 
 
         GateRepository gateRepository = new GateRepository(gateMap);
-        ParkingLotRepository parkingLotRepository = new ParkingLotRepository(); // Homework
+        ParkingLotRepository parkingLotRepository = new ParkingLotRepository(parkingLotMap);
         TicketRepository ticketRepository = new TicketRepository();
         VehicleRepository vehicleRepository = new VehicleRepository();
 
@@ -44,8 +79,18 @@ public class ParkingLotRunner {
         TicketController ticketController = new TicketController(ticketService);
 
         // Create objects of generateticketrequestdto and call generateTicket method;
+        GenerateTicketRequestDto requestDto = new GenerateTicketRequestDto();
+        requestDto.setGateId(1);
+        requestDto.setVehicleType(VehicleType.CAR.toString());
+        requestDto.setVehicleNumber("KA 05 1234");
+
+        GenerateTicketResponseDto responseDto = ticketController.generateTicket(requestDto);
+        System.out.println(responseDto);
 
 
+        requestDto.setVehicleNumber("KA 05 6789");
+        responseDto = ticketController.generateTicket(requestDto);
+        System.out.println(responseDto);
 
     }
 }
--- machine_coding/parking_lot/models/Spot.java
+++ machine_coding/parking_lot/models/Spot.java
@@ -1,6 +1,13 @@
 package machine_coding.parking_lot.models;
 
 public class Spot extends BaseModel{
+
+    public Spot(String name, SpotStatus status, VehicleType vehicleType) {
+        this.name = name;
+        this.status = status;
+        this.vehicleType = vehicleType;
+    }
+
     private String name;
     private SpotStatus status;
     private VehicleType vehicleType;
